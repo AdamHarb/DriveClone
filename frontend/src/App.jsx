520aed69e7aa1b89d6a159ac3b803c143b1b786a
Ayla Hmadi <108673836+Ayla-hmadi@users.noreply.github.com>
2024-04-16 16:14:35 +0300

    added signup route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
 import Login from "./components/Auth/Login/Login.jsx";
-
+import Signup from "./components/Auth/SignUp/Signup.jsx";
 function App() {
 
   return (
@@ -11,6 +11,7 @@ function App() {
           <Route path="/">
             <Route index element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="*" element={<h1> 404 page not found </h1>} />
           </Route>
         </Routes>
