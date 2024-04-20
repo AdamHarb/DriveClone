4ddc45a9fa6e4dbd639aecbf2691117998b5c89d
AliiceK <112477235+AliiceK@users.noreply.github.com>
2024-04-20 10:32:01 +0300

    Added form element

--- frontend/src/components/Auth/Login/Login.jsx
+++ frontend/src/components/Auth/Login/Login.jsx
@@ -4,19 +4,20 @@ const Login = () => {
     return (
         <div id="loginpage">
             <div id="login">
-                <div id="loginaccount">
-                    <div id="loginyouraccount">LOGIN YOUR ACCOUNT</div>
-                    <div class="fields">
-                        <label for="email">Email</label>
-                        <input  type="text" id="email" name="email" />
+                <form id='loginform'  >
+                    <div id="loginaccount">
+                        <div id="loginyouraccount">LOGIN YOUR ACCOUNT</div>
+                        <div class="fields">
+                            <label for="email">Email</label>
+                            <input type="text" id="email" name="email" />
+                        </div>
+                        <div class="fields">
+                            <label for="password"> Password</label>
+                            <input type="password" id="password" name="password" />
+                        </div>
+                        <button class="button" id="loginbutton"> Log In</button>
                     </div>
-                    <div class="fields">
-                        <label for="password"> Password</label>
-                        <input type="password" id="password" name="password" />
-                    </div>
-                    <button class="button" id="loginbutton"> Log In</button>
-                </div>
-
+                </form>
             </div>
             <br />
             <div id="signupoption">
