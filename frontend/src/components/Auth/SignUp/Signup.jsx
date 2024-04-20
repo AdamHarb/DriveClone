4bff59b9a4b97a1286ea4bf2ba949203f0adef27
AliiceK <112477235+AliiceK@users.noreply.github.com>
2024-04-20 10:35:25 +0300

    added the form element to signup and fixed some css

--- frontend/src/components/Auth/SignUp/Signup.jsx
+++ frontend/src/components/Auth/SignUp/Signup.jsx
@@ -3,26 +3,28 @@ import './Signup.css'
 const Signup = () => {
     return (
         <div id="signuppage">
-            <div id="signup">
-                <div id="signupaccount">
-                    <div id="createyouraccount"> CREATE YOUR ACCOUNT</div>
-                    <div class="fields">
-                        <label for="email">Email</label>
-                        <input  type="text" id="email" name="email" />
-                    </div>
+            <div id="signup" method='post'>
+                <form id="signupform">
+                    <div id="signupaccount">
+                        <div id="createyouraccount"> CREATE YOUR ACCOUNT</div>
+                        <div class="fields">
+                            <label for="email">Email</label>
+                            <input type="text" id="email" name="email" />
+                        </div>
 
-                    <div class="fields">
-                        <label for="password"> Password</label>
-                        <input type="password" id="password" name="password" />
-                    </div>
+                        <div class="fields">
+                            <label for="password"> Password</label>
+                            <input type="password" id="password" name="password" />
+                        </div>
 
-                    <div class="fields">
-                        <label for="confirmpassword">Confirm Password</label>
-                        <input  type="password" id="confirmpassword" name="confirmpassword" />
-                    </div>
+                        <div class="fields">
+                            <label for="confirmpassword">Confirm Password</label>
+                            <input type="password" id="confirmpassword" name="confirmpassword" />
+                        </div>
 
-                    <button class="button" id="signupbutton"> Sign up</button>
-                </div>
+                        <button class="button" id="signupbutton"> Sign up</button>
+                    </div>
+                </form>
 
             </div>
 
