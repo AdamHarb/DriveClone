57e73b631c619562f8ddc5469dd5c2249aec41f5
Adam Harb <aharbdev@outlook.com>
2024-04-24 23:09:46 +0300

    Refactored db connection and added bucket init

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,13 +1,36 @@
 const mongoose = require('mongoose');
+const { GridFSBucket } = require('mongodb');
 require('dotenv').config();
 
-async function main() {
+let db;
+let gfsBucket;
+
+async function connectDB() {
     try {
-        await mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(
+            process.env.ATLAS_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        );
         console.log("Connected to MongoDB Atlas via Mongoose");
+        db = mongoose.connection.db;
+        gfsBucket = new GridFSBucket(db);
     } catch (error) {
         console.error("Error connecting to MongoDB Atlas: ", error);
+        process.exit(1);
     }
 }
 
-module.exports = main;
+function getGridFSBucket() {
+    if (!gfsBucket) {
+        throw new Error("GridFS bucket not initialized");
+    }
+    return gfsBucket;
+}
+
+module.exports = {
+    connectDB,
+    getGridFSBucket,
+};
