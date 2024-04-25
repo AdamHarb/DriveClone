c65b3afe713391ff21bcbe0b9d045b5827a52bc2
joehage <joehage.2003@gmail.com>
2024-04-25 14:58:09 +0300

    Added the home route which returns folders and later will also return files

--- backend/API/controllers/FolderController.js
+++ backend/API/controllers/FolderController.js
@@ -1,2 +1,20 @@
 import File from '../models/File';
 import Folder from '../models/Folder';
+
+//When user logs in to website display his folders (This should be used with getFilesByUserId to get everything)
+exports.getFoldersByUserId = async (req, res) => {
+    try{
+        const user_id = req.params.user_id;
+        const folders = await Folder.find({user_id: user_id});
+        res.status(200).json(folders);
+    }
+    catch{
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+exports.createFolder = async (req, res) => {
+
+}
+
+
+
--- backend/API/routes.js
+++ backend/API/routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const userController = require("./controllers/UserController");
+const folderController = require("./controllers/FolderController");
+const getFoldersByUserId = require("./controllers/FolderController");
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
 			res.send('Hello World!');
 });
+//Add to this route the get files without folders api as well.
+router.get('/homepage', folderController.getFoldersByUserId);
+router.post('/createFolder', folderController.createFolder);
 
 router.get('/profile/:username', userController.getProfileByUsername);
+
 router.post('/createUser', userController.CreateUser);
 module.exports = router;
