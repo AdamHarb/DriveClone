1c60cf66dcbe4c5d0a702a31a0974ff2c204bc9c
joehage <joehage.2003@gmail.com>
2024-04-28 16:24:25 +0300

    No clue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
+import {CookiesProvider} from "react-cookie";
 
 const rootNode = document.getElementById('root');
 
 // eslint-disable-next-line react/no-deprecated
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, rootNode);
+ReactDOM.render(<React.StrictMode>
+	<CookiesProvider>
+	<App />
+	</CookiesProvider>
+</React.StrictMode>, rootNode);
