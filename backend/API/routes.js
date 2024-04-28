6539739da0988a2af0d731b06087de712a2d27a5
Adam Harb <aharbdev@outlook.com>
2024-04-29 02:52:21 +0300

    Added starring file function and route

--- backend/API/routes.js
+++ backend/API/routes.js
@@ -37,6 +37,8 @@ router.get('/details/:fileId', userAuth, fileController.getFileDetails);
 router.put('/update/:fileId', userAuth, fileController.updateFileDetails);
 router.delete('/delete-files/:fileId', userAuth, fileController.deleteFile);
 router.post('/rename-file', userAuth, fileController.renameFile);
+router.post('/star-file', userAuth, fileController.starFile);
+
 //Dashboard Routes
 router.get('/dashboard/:folderId', userAuth, driveController.getFolderStuff);
 router.get('/dashboard', userAuth, driveController.getRootStuff);
