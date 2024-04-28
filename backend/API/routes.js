b88028d3786ae1b1cf44676d4f588159c6a35708
joehage <joehage.2003@gmail.com>
2024-04-28 11:57:36 +0300

    Added dashboard API

--- backend/API/routes.js
+++ backend/API/routes.js
@@ -26,7 +26,7 @@ router.get('/login', userController.loginUser);
 router.post('/create-user', userController.createUser);
 
 // Folder Routes
-router.get('/dashboard', userAuth, folderController.getFoldersByUserId);
+router.get('/list-folders', userAuth, folderController.getFoldersByUserId);
 router.post('/create-folder', userAuth, folderController.createFolder);
 
 // File Routes
@@ -35,6 +35,16 @@ router.get('/download/:fileId', userAuth, fileController.downloadFile);
 router.get('/details/:fileId', userAuth, fileController.getFileDetails);
 router.put('/update/:fileId', userAuth, fileController.updateFileDetails);
 router.delete('/delete/:fileId', userAuth, fileController.deleteFile);
-router.get('/list', userAuth, fileController.listFiles);
+router.get('/list-files', userAuth, fileController.listFiles);
+
+//Dashboard Routes
+router.get('/dashboard', userAuth, function (req, res) {
+		const userFolders = folderController.getFoldersByUserId;
+		const userFiles = fileController.listFiles;
+		res.json({
+			userFolders,
+			userFiles
+		});
+});
 
 module.exports = router;
