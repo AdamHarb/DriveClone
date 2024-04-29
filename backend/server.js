2384f819ee74bc5eccd227ca92b4b36d4c2b80c2
Adam Harb <aharbdev@outlook.com>
2024-04-30 02:35:48 +0300

    Configured server connection to listen to dynamic port

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const storage = multer.memoryStorage();
 multer({ storage: storage });
 
 connectDB().catch(console.error).then(() => {
-	app.listen(3000, () => {
-		console.log('Server is running on http://localhost:3000');
-		console.log('API is running on http://localhost:3000/api/');
+	app.listen(process.env.PORT, () => {
+		console.log('Server is running on http://localhost:' + process.env.PORT);
+		console.log('API is running on http://localhost:' + process.env.PORT + '/api');
 	});
 });
